1f2842131c75123a67d4f6afbc7467b2b57693e4
Micaela Jenik <micaela.jenik@gmail.com>
2021-01-28 19:00:34 -0300

    added the show view and method, upgraded index and home views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :artworks, only: :index
+  resources :artworks, only: [:index, :show]
   patch '/artworks/:artwork_id', to: 'artworks#buy', as: 'buy_artwork'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
