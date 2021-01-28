d5431c021e12d11f42dc57a75f8540c2e67f0537
Claudio Tome <cptome@gmail.com>
2021-01-28 18:54:59 -0300

    brach del new implementado

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -2,4 +2,25 @@ class ArtworksController < ApplicationController
   def index
     @artworks = Artwork.all
   end
+
+  def new
+    @artwork = Artwork.new
+  end
+ 
+  def create
+    @artwork = Artwork.new(artwork_params)
+     @artwork.user = current_user
+    if @artwork.save
+      redirect_to artworks_path(@artwork)
+    else
+      render :new
+    end
+  end
+ 
+  private
+ 
+  def artwork_params
+    params.require(:artwork).permit(:title, :technique, photos:[])
+  end
+
 end
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,6 +1,7 @@
 class Artwork < ApplicationRecord
   TECHNIQUES = %w(sculpture painting digital)
   belongs_to :user
+  has_many_attached :photos
 
   validates :title, presence: true
   validates :technique, inclusion: TECHNIQUES
