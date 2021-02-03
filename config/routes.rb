cc0fdc89ba7c081bebaa2780e49b77222db63de6
Guido Tibaudin <guidotibaudin@gmail.com>
2021-02-02 22:16:28 -0300

    Implemented My artworks and put/remove from sale

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#welcome'
   get '/home', to: 'pages#home'
-  resources :artworks, only: [:index, :new, :create, :show, :purchase] do
+  get 'users/:id/my_artworks', to: 'artworks#my_artworks', as: 'user_artworks'
+  patch 'users/:id/my_artworks/:id', to: 'artworks#toggle_sale', as: 'artwork_toggle_sale'
+  resources :artworks, only: [:index, :new, :create, :show, :purchase, :edit, :update] do
     resources :purchases, only: [:new, :create]
   end
 
   resources :purchases, only: [] do
     resources :reviews, only: [:new, :create]
   end
-  # get '/artworks/:artwork_id/purchase/', to: 'purchases#buy', as: 'buy'
-  # post '/artworks/:artwork_id/purchase/', to: 'purchases#create', as: 'artwork_purchases'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
